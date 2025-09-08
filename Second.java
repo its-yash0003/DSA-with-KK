357c9d13978662d158af2bff630dd2abbf09ecef
YASH GAUTAM <152496325+its-yash0003@users.noreply.github.com>
2025-09-09 00:01:20 +0530

    Update question 5 and calculator program in Second.java

--- Second.java
+++ Second.java
@@ -151,10 +151,54 @@ public class Second {
 //        System.out.println(count);
 
 //         question 5
+//          int num = 546343;
+//          int ans = 0;
+//          while ( num > 0){
+//              int rem = num % 10;
+//          num /= 10;
+//              ans = ans * 10 + rem;
+//          }
+//        System.out.println(ans);
 
 
-
-
+//        calculator
+//        Scanner in = new Scanner(System.in);
+//        // take input from user till user does not pree X or x
+//        int ans = 0 ;
+//        while (true) {
+//            //take the operator as input
+//            System.out.print(" Entre the operator: ");
+//            char op = in.next().trim().charAt(0);
+//            if (op == '+' ||op == '*' ||op == '-' ||op == '/' ||op == '%' ){
+//                // input 2 no's
+//                System.out.print("Entre the numbers : ");
+//                int num1 = in.nextInt();
+//                int num2 = in.nextInt();
+//
+//            if (op == '+' ){
+//                ans = num1 + num2;
+//            }
+//            if (op == '-' ){
+//                ans = num1 - num2;
+//            }
+//            if (op == '*' ){
+//                ans = num1 * num2;
+//            }
+//            if (op == '/' ){
+//                if (num2 != 0){
+//                ans = num1 / num2;
+//                }
+//            }
+//            if (op == '%' ) {
+//                ans = num1 % num2;
+//            }
+//            }else if (op == 'X' || op == 'x' ){
+//                break;
+//            }else {
+//                System.out.println("Invalid operation !!");
+//            }
+//            System.out.println(ans);
+//        }
 
 
     }
